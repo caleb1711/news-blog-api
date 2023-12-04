b6b37de690df199642611fbdeef0e64387378726
caleb1711 <calkra15@outlook.com>
2023-12-05 03:45:23 +0500

    update procfile

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT) + [
+urlpatterns = static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + [
     path("api/", include("api.urls")),
     path('', admin.site.urls),
     
