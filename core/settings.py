7866638330329d2d794dcaffe692bb0ac435ce51
caleb1711 <calkra15@outlook.com>
2023-12-05 03:11:50 +0500

    Re-factor

--- core/settings.py
+++ core/settings.py
@@ -122,9 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# FOR LIVE SERVER 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# media 
+# Base url to serve media files
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
